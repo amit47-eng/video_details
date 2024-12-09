fb9f8b9f6ce49e0c1bc711b2c025edf1d77575ce
amit47-eng <av495267@gmail.com>
2024-12-09 21:52:19 +0530

    change

--- src/Form.jsx
+++ src/Form.jsx
@@ -3,12 +3,10 @@ import { useRef } from "react"
 function Form(){
     const userRef =useRef(null)
     const pssRef = useRef(null)
-
     const handleSubmit = (event)=>{
         event.preventDefault();
         console.log(useRef.current.value);
-        console.log(pssRef.current.value);
-        
+        console.log(pssRef.current.value); 
     }
     return(
         <form onSubmit={handleSubmit}>
@@ -17,7 +15,6 @@ function Form(){
             <input ref={pssRef} type="password" placeholder="password" />
             <button ref={btnRef} >cleck</button>
         </form>
-        
     )
 }
 export default Form;
